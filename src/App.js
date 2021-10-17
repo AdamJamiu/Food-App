983f241f6da899688c433e36cf3059faebe879fd
AdamJamiu <adamjamiu98@gmail.com>
2021-10-17 17:12:31 +0100

    verify account div

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import React from "react";
-import { Container, Button, Stack, ChakraProvider, Text} from "@chakra-ui/react";
+import {
+  Container,
+  Button,
+  ChakraProvider,
+  Text,
+  Stack,
+  Box
+} from "@chakra-ui/react";
+import { Verify } from './Components';
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
-
 
 export default function App() {
   return (
     <ChakraProvider>
- 
-    <Button colorScheme="blue">Logout</Button>
-    <Container maxW="container.xlg">
-    <Stack spacing={4} direction="row" align="center">
-    <Button colorScheme="blue">1</Button><Text>Verify Account</Text>
-    <Button colorScheme="blue">2</Button> <Text>Verify Account</Text>
+      <Container maxW="container.lg" pt={30} >
+     <Box textAlign='right' mb='10'>
+        <Button color='#fff' bg='#FF6774'>Logout</Button>
+        </Box>
+        <Stack  pb='4' spacing={20} direction='row' alignItems="center" width='100%' borderBottom='1px solid #E9EEF4'>
+      
+          <Stack direction='row' align='center' spacing={4}>
+          <Button colorScheme="blue">1</Button>
+          <Text fontSize='xl'>Verify Account</Text>
+          </Stack>
 
-    </Stack>
-  </Container>
-  </ChakraProvider>
-    );
+          <Stack direction='row' align='center' spacing={4}>
+          <Button colorScheme="blue">2</Button>
+           <Text  fontSize='xl'>Social Handles</Text>
+          </Stack>
+
+          <Stack direction='row' align='center' spacing={4} >
+          <Button colorScheme="blue">3</Button>
+           <Text fontSize='xl'>Business Category</Text>
+          </Stack>
+
+        </Stack>
+
+             <Verify/>
+      
+      </Container>
+    </ChakraProvider>
+  );
 }
--- src/Components.js
+++ src/Components.js
@@ -1,8 +1,96 @@
-import React from 'react';
+import React from "react";
+import "./index.css";
+import {
+  Box,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionIcon,
+  AccordionPanel,
+  Input,
+  UnorderedList,
+  ListItem,
+  Button,
+  HStack,
+  VStack,
+  Stack,
+  Text,
+  Heading,
+} from "@chakra-ui/react";
+import { render } from "react-dom";
 
+import { CheckIcon } from '@chakra-ui/icons';
 
-export function Component() {
-    return (
-        <div></div>
-    )
+export function Verify() {
+  return (
+    <Stack pt={5} mb={12}>
+      <Text color="var(--google-grey)">Step 1/3</Text>
+      <Heading size="lg" mt={4} pb="55px" color="var(--dark)">
+        Verify Account
+      </Heading>
+
+      <Stack>
+        <Text as="h4" fontSize={12}>
+          Select a verification method
+        </Text>
+        <Stack spacing={5} direction="row">
+          <Button background="var(--gray-bg)">BVN</Button>
+          <Button border="0.1px solid var(--google-gray)">
+            Personal Account Number
+          </Button>
+        </Stack>
+        <BvnVerify />
+      </Stack>
+    </Stack>
+  );
+}
+
+export function BvnVerify() {
+  return (
+    <>
+      <VStack spacing={5} align="left" pt={6}>
+        <Heading size="sm">Bank Verification Number (11-digits)</Heading>
+        <Input size="lg" />
+      </VStack>
+
+      <Accordion background='var(--grey)' padding='1em' mt="20px">
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box flex="1" textAlign="left">
+                Section 1 title
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel color='var(--dark)' pb={4}>
+              <Text mb={5}>We only need your access to your</Text>
+            <UnorderedList>
+              <Stack direction='row' align='center' spacing={2}>
+              <CheckIcon/>
+              <ListItem> Lorem ipsum dolor sit amet</ListItem>
+              </Stack>
+              <Stack direction='row' align='center' spacing={2}>
+              <CheckIcon/>
+              <ListItem> Lorem ipsum dolor sit amet</ListItem>
+              </Stack>
+              <Stack direction='row' align='center' spacing={2}>
+              <CheckIcon/>
+              <ListItem> Lorem ipsum dolor sit amet</ListItem>
+              </Stack>
+              <Stack direction='row' align='center' spacing={2}>
+              <CheckIcon/>
+              <ListItem> Lorem ipsum dolor sit amet</ListItem>
+              </Stack>
+              <Stack direction='row' align='center' spacing={2}>
+              <CheckIcon/>
+              <ListItem> Lorem ipsum dolor sit amet</ListItem>
+              </Stack>
+                
+            </UnorderedList>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+    </>
+  );
 }
