9f114e77c6f5fab35615aa4cb0fa4ae1929cc6b8
AdamJamiu <adamjamiu98@gmail.com>
2021-10-18 23:05:17 +0100

    updte AApp.js

--- src/App.js
+++ src/App.js
@@ -8,10 +8,9 @@ import {
   Box,
 } from "@chakra-ui/react";
 import { SocialHandles, Verify, BusinessCat } from "./Components";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom";
 
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
 
 export default function App() {
   return (
@@ -32,47 +31,42 @@ export default function App() {
             borderBottom="1px solid #E9EEF4"
           >
             <Link transition="glide-right" to="verifyBVN">
-            <Stack direction="row" align="center" spacing={4}>
-              <Button colorScheme="blue">1</Button>
-              <Text fontSize="xl">Verify Account</Text>
-            </Stack>
+              <Stack direction="row" align="center" spacing={4}>
+                <Button colorScheme="blue">1</Button>
+                <Text fontSize="xl">Verify Account</Text>
+              </Stack>
             </Link>
 
             <Link to="socialHandles" transition="glide-right">
-            <Stack direction="row" align="center" spacing={4}>
-              <Button colorScheme="blue">2</Button>
-              <Text fontSize="xl">Social Handles</Text>
-              
-            </Stack>
+              <Stack direction="row" align="center" spacing={4}>
+                <Button colorScheme="blue">2</Button>
+                <Text fontSize="xl">Social Handles</Text>
+              </Stack>
             </Link>
 
             <Link to="BusinessCategory" transition="glide-right">
-            <Stack direction="row" align="center" spacing={4}>
-              <Button colorScheme="blue">3</Button>
-              <Text fontSize="xl">Business Category</Text>
-            </Stack>
+              <Stack direction="row" align="center" spacing={4}>
+                <Button colorScheme="blue">3</Button>
+                <Text fontSize="xl">Business Category</Text>
+              </Stack>
             </Link>
-            
           </Stack>
-         
 
-          <Switch> 
-          
-          <Route exact path="/verifyBVN">
-            <Verify />
-          </Route>
-          <Route path="/socialHandles">
-            <SocialHandles />
-          </Route>
-          <Route path="/BusinessCategory">
-            <BusinessCat />
-          </Route>
-        </Switch>
-
-      
+          <Switch>
+            <Route exact path="/">
+              <Redirect to="/verifyBVN" />
+            </Route>
+            <Route path="/verifyBVN">
+              <Verify />
+            </Route>
+            <Route path="/socialHandles">
+              <SocialHandles />
+            </Route>
+            <Route path="/BusinessCategory">
+              <BusinessCat />
+            </Route>
+          </Switch>
         </Container>
-
-       
       </Router>
     </ChakraProvider>
   );
